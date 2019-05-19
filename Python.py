36e155f22e8bc4d48ef66a15f26acad0c1a48931
Dennison Bechis <Dennison.Bechis1@gmail.com>
2019-05-18 18:33:11 -0600

    Fix of bugs

--- Python.py
+++ Python.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+a = [1,2,3,4,5,6]
+b = np.mean(a)
+print(b)
+
 destinations = ["Paris, France","Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 def get_destination_index(destination):
@@ -33,6 +39,22 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination,interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for i in attractions_in_city:
+    possible_attraction = i
+    attraction_tags = i[1]
+    for interest in interests:
+      if interest == attraction_tags[0]:
+        attractions_with_interest.append(possible_attraction[0])
+  return attractions_with_interest
+  
+la_arts = find_attractions('Los Angeles, USA',['art'])
+print(la_arts)
+  
 
 
 
